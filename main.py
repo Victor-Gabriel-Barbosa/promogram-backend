c5978347b5e499ff6887678ef173832e09e61c11
victor <victorgabrielbarbosa88@gmail.com>
2026-08-24 14:14:41 -0300

    refactor: Melhora a formatação e a consistência do código nas funções de manipulação de produtos e cupons

--- main.py
+++ main.py
@@ -29,91 +29,67 @@ app.add_middleware(
 )
 
 DbSession = Annotated[Session, Depends(get_db)]
-
-
+ 
 @app.post("/produtos", status_code=status.HTTP_201_CREATED)
-async def criar_produto(produto: Produto, db: Session = DbSession):
-    novo_produto = models.Produto(**produto.model_dump())
-    db.add(novo_produto)
-    db.commit()
-    db.refresh(novo_produto)
-    print(produto)
-    return {"Produto": novo_produto}
-
+async def criar_produto(produto: Produto, db: DbSession):
+  novo_produto = models.Produto(**produto.model_dump())
+  db.add(novo_produto)
+  db.commit()
+  db.refresh(novo_produto)
+  print(produto)
+  return {"Produto": novo_produto}
 
 @app.post("/cupons", status_code=status.HTTP_201_CREATED)
-async def criar_cupom(cupom: Cupom, db: Session = DbSession):
-    novo_cupom = models.Cupom(**cupom.model_dump())
-    db.add(novo_cupom)
-    db.commit()
-    db.refresh(novo_cupom)
-    print(cupom)
-    return {"Cupom": novo_cupom}
+async def criar_cupom(cupom: Cupom, db: DbSession):
+  novo_cupom = models.Cupom(**cupom.model_dump())
+  db.add(novo_cupom)
+  db.commit()
+  db.refresh(novo_cupom)
+  print(cupom)
+  return {"Cupom": novo_cupom}
 
-
-@app.get(
-    "/produtos", response_model=list[ProdutoResponse], status_code=status.HTTP_200_OK
-)
-async def buscar_produtos(db: Session = DbSession, skip: int = 0, limit: int = 100):
-    return db.query(models.Produto).offset(skip).limit(limit).all()
-
+@app.get("/produtos", response_model=list[ProdutoResponse], status_code=status.HTTP_200_OK)
+async def buscar_produtos(db: DbSession, skip: int = 0, limit: int = 100):
+  return db.query(models.Produto).offset(skip).limit(limit).all()
 
 @app.get("/cupons", response_model=list[CupomResponse], status_code=status.HTTP_200_OK)
-async def buscar_cupons(db: Session = DbSession, skip: int = 0, limit: int = 100):
-    return db.query(models.Cupom).offset(skip).limit(limit).all()
+async def buscar_cupons(db: DbSession, skip: int = 0, limit: int = 100):
+  return db.query(models.Cupom).offset(skip).limit(limit).all()
 
+@app.put("/produtos/{produto_id}", response_model=ProdutoResponse, status_code=status.HTTP_200_OK)
+async def atualizar_produto(produto_id: int, produto: Produto, db: DbSession):
+  produto_query = db.query(models.Produto).filter(models.Produto.id == produto_id)
+  produto_existente = produto_query.first()
+  if produto_existente is None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
+  produto_query.update(produto.model_dump(), synchronize_session=False)
+  db.commit()
+  return produto_query.first()
 
-@app.put(
-    "/produtos/{produto_id}",
-    response_model=ProdutoResponse,
-    status_code=status.HTTP_200_OK,
-)
-async def atualizar_produto(produto_id: int, produto: Produto, db: Session = DbSession):
-    produto_query = db.query(models.Produto).filter(models.Produto.id == produto_id)
-    produto_existente = produto_query.first()
-    if produto_existente is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado"
-        )
-    produto_query.update(produto.model_dump(), synchronize_session=False)
-    db.commit()
-    return produto_query.first()
-
-
-@app.put(
-    "/cupons/{cupom_id}", response_model=CupomResponse, status_code=status.HTTP_200_OK
-)
-async def atualizar_cupom(cupom_id: int, cupom: Cupom, db: Session = DbSession):
-    cupom_query = db.query(models.Cupom).filter(models.Cupom.id == cupom_id)
-    cupom_existente = cupom_query.first()
-    if cupom_existente is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado"
-        )
-    cupom_query.update(cupom.model_dump(), synchronize_session=False)
-    db.commit()
-    return cupom_query.first()
-
-
+@app.put("/cupons/{cupom_id}", response_model=CupomResponse, status_code=status.HTTP_200_OK)
+async def atualizar_cupom(cupom_id: int, cupom: Cupom, db: DbSession):
+  cupom_query = db.query(models.Cupom).filter(models.Cupom.id == cupom_id)
+  cupom_existente = cupom_query.first()
+  if cupom_existente is None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado")
+  cupom_query.update(cupom.model_dump(), synchronize_session=False)
+  db.commit()
+  return cupom_query.first()
+  
 @app.delete("/produtos/{produto_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def remover_produto(produto_id: int, db: Session = DbSession):
-    produto_query = db.query(models.Produto).filter(models.Produto.id == produto_id)
-    if produto_query.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado"
-        )
-    produto_query.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
+async def remover_produto(produto_id: int, db: DbSession):
+  produto_query = db.query(models.Produto).filter(models.Produto.id == produto_id)
+  if produto_query.first() is None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Produto não encontrado")
+  produto_query.delete(synchronize_session=False)
+  db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.delete("/cupons/{cupom_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def remover_cupom(cupom_id: int, db: Session = DbSession):
-    cupom_query = db.query(models.Cupom).filter(models.Cupom.id == cupom_id)
-    if cupom_query.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado"
-        )
-    cupom_query.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+async def remover_cupom(cupom_id: int, db: DbSession):
+  cupom_query = db.query(models.Cupom).filter(models.Cupom.id == cupom_id)
+  if cupom_query.first() is None:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cupom não encontrado")
+  cupom_query.delete(synchronize_session=False)
+  db.commit()
+  return Response(status_code=status.HTTP_204_NO_CONTENT)
